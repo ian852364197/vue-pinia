580cc96d5d2d2fa92aa3ac306bae7ce75f90f7fa
IanTang <iantang@uitc.com.tw>
2024-07-29 16:54:22 +0800

    feat. 登入時counter也能持久化

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,20 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
+import { useUserStore } from './user';
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0);
-  const doubleCount = computed(() => count.value * 2);
-  function increment() {
-    count.value++;
+export const useCounterStore = defineStore(
+  'counter',
+  () => {
+    const count = ref(0);
+    const doubleCount = computed(() => count.value * 2);
+    function increment() {
+      count.value++;
+    }
+
+    return { count, doubleCount, increment };
+  },
+  {
+    //登入時counter也能持久化
+    persist: useUserStore().isSignIn
   }
-
-  return { count, doubleCount, increment };
-});
+);
